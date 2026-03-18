68c818d8352103bbb451eb3ad1a612cef03b473f
Onigiripurin <onigiri1995@outlook.jp>
2026-03-18 16:14:35 +0000

    feat: ReactRouterをセットアップ

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,5 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
 
 const root = document.getElementById("root");
 
@@ -9,6 +11,8 @@ if (!root) {
 
 createRoot(root).render(
 	<StrictMode>
-		<p style={{ fontSize: "4rem", fontWeight: "bold" }}>Hello World</p>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
 	</StrictMode>,
 );
