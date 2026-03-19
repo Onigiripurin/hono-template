ec859f8173cb10ff6cbd3958b9038c5280c24c63
Onigiripurin <onigiri1995@outlook.jp>
2026-03-19 02:53:01 +0000

    feat: Post: Userエンドポイントを定義

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,23 @@
+import { CreateUserUseCase } from "@repo/ts-server-application";
+import { UserRepositoryImpl } from "@repo/ts-server-prisma-adapter";
 import { Hono } from "hono";
+
+const userRepository = new UserRepositoryImpl();
+const createUserUseCase = new CreateUserUseCase(userRepository);
 
 const app = new Hono();
 
 app.get("/", (c) => {
 	return c.text("Hello Hono!");
+});
+
+app.post("/users", async (c) => {
+	const body = await c.req.json<{ name: string; email: string }>();
+	const user = await createUserUseCase.execute({
+		name: body.name,
+		email: body.email,
+	});
+	return c.json(user, 201);
 });
 
 export default app;
